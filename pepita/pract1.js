89aca938f6193d15f81d8c0d7c42455865adb388
DianaAndújar <dianaandujarg@gmail.com>
2021-02-26 15:22:11 -0400

    listo

---
+++ pepita/pract1.js
@@ -0,0 +1,14 @@
+let albert = prompt("uno");
+parseInt(albert);
+let gregorio = prompt("do");
+parseInt(gregorio);
+if (gregorio != 0)
+{
+francisco = albert/gregorio;
+document.writeln("es..." + francisco);
+}
+else if(gregorio == 0)
+{
+    document.writeln("que no otrave");
+}
+
---
+++ pepita/pract4.js
@@ -0,0 +1,9 @@
+let teamb = prompt("ecribe klk");
+if(teamb == "dipre" || teamb == "e"|| teamb=="i"||teamb=="o"|| teamb=="u")
+{
+    document.writeln("Vocal");
+}
+else 
+{
+    document.write("caracter");
+}
