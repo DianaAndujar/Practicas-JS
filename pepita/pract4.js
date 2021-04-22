0192ca37492f0c35d83200d1cccd786d4c82061b
DianaAndujar <75079843+DianaAndujar@users.noreply.github.com>
2021-04-22 10:22:45 -0400

    Delete pepita directory

--- pepita/pract4.js
+++
@@ -1,9 +0,0 @@
-let teamb = prompt("ecribe klk");
-if(teamb == "dipre" || teamb == "e"|| teamb=="i"||teamb=="o"|| teamb=="u")
-{
-    document.writeln("Vocal");
-}
-else 
-{
-    document.write("caracter");
-}
